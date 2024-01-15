3a1b8b0afa559ca69cbf84f96803db599504bf3c
Alexa <kusteralexa@gmail.com>
2024-01-14 22:19:26 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import { Navbar } from './screens';
 
 //https://www.favicon-generator.org/
 
 function App() {
   return (
     <div>
-   
+      <Navbar />
     </div>
   );
 }
--- src/screens/Navbar/Navbar.jsx
+++ src/screens/Navbar/Navbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import "./Navbar.css";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <div className='navbar-name'>Alexa Kuster</div>
+      <div className='navbar-links-container'>
+        <a>Home</a>
+        <a>About</a>
+        <a>Technologies</a>
+        <a>Portfolio</a>
+        <a>Contact</a>
+      </div>
+    </div>
   )
 }
 
