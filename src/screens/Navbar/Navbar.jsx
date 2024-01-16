2b18748633f4c03f64608d292209232d84a7b41a
Alexa <kusteralexa@gmail.com>
2024-01-16 09:26:30 -0500

    began styling navbar

--- src/screens/Navbar/Navbar.jsx
+++ src/screens/Navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./Navbar.css";
+import hamBtn from "./hamburger-menu (1).svg"
 
 function Navbar() {
   return (
+
     <div className='navbar-container'>
-      <div className='navbar-name'>Alexa Kuster</div>
+      <div className='navbar-name'>
+        <div>Alexa Kuster</div>
+          <img alt='hamburger menu' src={hamBtn} />
+      </div>
+      
       <div className='navbar-links-container'>
         <a href='#home'>Home</a>
         <a href='#about'>About</a>
