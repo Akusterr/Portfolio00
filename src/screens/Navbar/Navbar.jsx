323294fa79fd39752e7b9e23be197a3afb7b8092
Alexa <kusteralexa@gmail.com>
2024-01-17 11:06:32 -0500

    finishing touches on Navbar

--- src/screens/Navbar/Navbar.jsx
+++ src/screens/Navbar/Navbar.jsx
@@ -1,23 +1,49 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
 import "./Navbar.css";
 import hamBtn from "./hamburger-menu (1).svg"
 
 function Navbar() {
+    const[showLinks, setShowLinks] = useState(true);
+    const [showBtn, setShowBtn] = useState(false);
+
+    const handleClick = () => setShowLinks(!showLinks);
+    
+    window.addEventListener("resize", (e) => {
+      if (e.target.innerWidth > 768) {
+        setShowBtn(false);
+        setShowLinks(true);
+      } else {
+        setShowBtn(true);
+        setShowLinks(false);
+      }
+    })
+
+    useEffect (() => {
+      if(window.innerWidth > 768) {
+        setShowBtn(false);
+        setShowLinks(true);
+      } else {
+        setShowBtn(true);
+        setShowLinks(false);
+      }
+    }, [])
+
   return (
 
     <div className='navbar-container'>
       <div className='navbar-name'>
         <div>Alexa Kuster</div>
-          <img alt='hamburger menu' src={hamBtn} />
+        {showBtn && <img alt='hamburger menu' src={hamBtn} onClick={handleClick}/>}
       </div>
-      
-      <div className='navbar-links-container'>
-        <a href='#home'>Home</a>
-        <a href='#about'>About</a>
-        <a href='#technologies'>Technologies</a>
-        <a href='#portfolio'>Portfolio</a>
-        <a href='#contact'>Contact</a>
-      </div>
+      {showLinks && (
+        <div className='navbar-links-container'>
+          <a href='#home'>Home</a>
+          <a href='#about'>About</a>
+          <a href='#technologies'>Technologies</a>
+          <a href='#portfolio'>Portfolio</a>
+          <a href='#contact'>Contact</a>
+        </div>
+      )}
     </div>
   )
 }
