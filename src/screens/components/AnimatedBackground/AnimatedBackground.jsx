25aa1421af2cc3decde919251c3acfe86f740e83
Alexa <kusteralexa@gmail.com>
2024-01-25 19:43:59 -0500

    created animated background loop

--- src/screens/components/AnimatedBackground/AnimatedBackground.jsx
+++ src/screens/components/AnimatedBackground/AnimatedBackground.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 import { ReactComponent as Innotation } from "./svg/innotation.svg";
 import { ReactComponent as Automation } from "./svg/automation.svg";
 import { ReactComponent as Debug } from "./svg/debug.svg";
-import { ReactComponent as Checklist } from "./svg/checklist.svg";
 import { ReactComponent as Code1 } from "./svg/code1.svg";
-import { ReactComponent as Code2 } from "./svg/code2.svg";
-import { ReactComponent as CodeSquare } from "./svg/codeSquare.svg";
 import { ReactComponent as Code3 } from "./svg/code3.svg";
 import { ReactComponent as DataCode } from "./svg/dataCode.svg";
 import { ReactComponent as Data1 } from "./svg/data1.svg";
@@ -34,25 +31,42 @@ function AnimatedBackground( {children}) {
     <div key={i} className="animated-row">
       <div>
         <Innotation />
-        <Automation />
-        <Debug />
-        <Checklist />
-        <Code1 />
-        <Code2 />
+        <Lotus />
+        <Github2 />
         <Code3 />
-        <CodeSquare />
+        <Java1 />
+        <Github1 />
         <DataCode />
         <Data1 />
         <Download1 />
-        <FingerClick />
-        <Github1 />
-        <Github2 />
-        <Java1 />
-        <Lotus />
-        <Memo />
+        <Automation />
+        <Code1 />
         <Program />
+        <Memo />
+        <Debug />
         <RectangleCode />
         <Software />
+        <FingerClick />
+      </div>
+
+      <div>
+        <Innotation />
+        <Lotus />
+        <Github2 />
+        <Code3 />
+        <Java1 />
+        <Github1 />
+        <DataCode />
+        <Data1 />
+        <Download1 />
+        <Automation />
+        <Code1 />
+        <Program />
+        <Memo />
+        <Debug />
+        <RectangleCode />
+        <Software />
+        <FingerClick />
       </div>
     </div>
   ))
