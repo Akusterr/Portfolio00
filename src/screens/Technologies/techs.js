fb342e035489c0fe92acb891b0ba69bfa948468d
Alexa <kusteralexa@gmail.com>
2024-02-18 09:33:30 -0500

    Fixed bug & completed the Technologies page

--- src/screens/Technologies/techs.js
+++ src/screens/Technologies/techs.js
@@ -1,7 +1,28 @@
-import api1 from "./svg/api1.svg";
-import api2 from "./svg/api2.svg";
+import api from "./svg/api.svg";
+import css from "./svg/css.svg";
+import debug from "./svg/debug.svg";
+import git from "./svg/git.svg";
+import github from "./svg/github.svg";
+import html from "./svg/html.svg";
+import JS from "./svg/JS.svg";
+import json from "./svg/json.svg";
+import react from "./svg/react.svg";
+import ruby from "./svg/ruby.svg";
+import TS from "./svg/TS.svg";
+import SQL from "./svg/SQL.svg";
+
 
 export const techs = [
-    {name: "api1", image: api1},
-    {name: "api2", image: api2}
+    {name: "API", image: api},
+    {name: "CSS", image: css},
+    {name: "Debug", image: debug},
+    // {name: "git", image: git},
+    {name: "Github", image: github},
+    {name: "HTML", image: html},
+    {name: "JS", image: JS},
+    // {name: "json", image: json},
+    {name: "React", image: react},
+    {name: "Ruby", image: ruby},
+    {name: "TS", image: TS},
+    {name: "SQL", image: SQL},
 ]
